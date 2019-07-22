ebd1e26e464cb39c8042f621358954f59d4cb200
renkai.von <renkai.von@bytedance.com>
2019-07-22 10:26:26 +0800

    init project.

---
+++ BigTerrain/Assets/Scripts/TerrainCuttingEditor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace playerone.unity { 
+
+    public class TerrainCuttingEditor : Editor {
+
+        private static bool onlyStatic = true;
+
+        [MenuItem("Terrain/Cutting")]
+        private static void CuttingTerrain()
+        {
+            GameObject[] roots = EditorSceneManager.GetActiveScene().GetRootGameObjects();
+            List<GameObject> objDealing = new List<GameObject>();
+            foreach(GameObject root in roots)
+            {
+                travelGameObjects(root, objDealing);
+            }
+        }
+
+        private static void travelGameObjects(GameObject root, List<GameObject> objDealing)
+        {
+            if (!root.activeSelf)
+            {
+                return;
+            }
+
+            if ((root.GetComponent<MeshRenderer>() || root.GetComponent<Terrain>()) && (!onlyStatic || (onlyStatic && root.isStatic)))
+            {
+                objDealing.Add(root);
+                return;
+            }
+        }
+    }
+
+}
